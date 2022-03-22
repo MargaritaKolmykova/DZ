839047a4387641a6ad6a673b516f5ab527ec08fd
MargaritaKolmykova <GGhub2022!17>
2022-03-22 12:58:39 +0300

    v2

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def start(message, res=False):
     chat_id = message.chat.id
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("👋 Главное меню")
-    btn2 = types.KeyboardButton("❓ Помощь")
+    btn1 = types.KeyboardButton("✌️ Главное меню")
+    btn2 = types.KeyboardButton("💬 Помощь")
     markup.add(btn1, btn2)
 
     bot.send_message(chat_id,
-                     text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке ПаЙтон".format(
+                     text="Привет, {0.first_name} 😉 Я тестовый бот для курса программирования на языке ПаЙтон 🐍".format(
                          message.from_user), reply_markup=markup)
 
 
@@ -28,41 +28,41 @@ def get_text_messages(message):
     chat_id = message.chat.id
     ms_text = message.text
 
-    if ms_text == "Главное меню" or ms_text == "👋 Главное меню" or ms_text == "Вернуться в главное меню":  # ..........
+    if ms_text == "Главное меню" or ms_text == "✌️ Главное меню" or ms_text == "Вернуться в главное меню":  # ..........
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Развлечения")
-        btn2 = types.KeyboardButton("WEB-камера")
-        btn3 = types.KeyboardButton("Управление")
-        back = types.KeyboardButton("Помощь")
+        btn1 = types.KeyboardButton("🎉 Развлечения")
+        btn2 = types.KeyboardButton("🖥 WEB-камера")
+        btn3 = types.KeyboardButton("💡 Управление")
+        back = types.KeyboardButton("💬 Помощь")
         markup.add(btn1, btn2, btn3, back)
         bot.send_message(chat_id, text="Вы в главном меню", reply_markup=markup)
 
-    elif ms_text == "Развлечения":  # ..................................................................................
+    elif ms_text == "🎉 Развлечения":  # ..................................................................................
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Прислать собаку")
-        btn2 = types.KeyboardButton("Прислать анекдот")
+        btn1 = types.KeyboardButton("🐶 Прислать собаку")
+        btn2 = types.KeyboardButton("😂 Прислать анекдот")
         back = types.KeyboardButton("Вернуться в главное меню")
         markup.add(btn1, btn2, back)
-        bot.send_message(chat_id, text="Развлечения", reply_markup=markup)
+        bot.send_message(chat_id, text="Выбирай...", reply_markup=markup)
 
-    elif ms_text == "/dog" or ms_text == "Прислать собаку":  # .........................................................
+    elif ms_text == "/dog" or ms_text == "🐶 Прислать собаку":  # .........................................................
+        bot.send_message(chat_id, text="еще не готово...😢")
+
+    elif ms_text == "😂 Прислать анекдот":  # .............................................................................
         bot.send_message(chat_id, text="еще не готово...")
 
-    elif ms_text == "Прислать анекдот":  # .............................................................................
+    elif ms_text == "🖥 WEB-камера":
         bot.send_message(chat_id, text="еще не готово...")
 
-    elif ms_text == "WEB-камера":
+    elif ms_text == "💡 Управление":  # ...................................................................................
         bot.send_message(chat_id, text="еще не готово...")
 
-    elif ms_text == "Управление":  # ...................................................................................
-        bot.send_message(chat_id, text="еще не готово...")
-
-    elif ms_text == "Помощь" or ms_text == "/help":  # .................................................................
-        bot.send_message(chat_id, "Автор: Я")
+    elif ms_text == "💬 Помощь" or ms_text == "/help":  # .................................................................
+        bot.send_message(chat_id, "Автор: Рита Колмыкова")
         key1 = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton(text="Напишите автору", url="https://t.me/BIaDISlav")
+        btn1 = types.InlineKeyboardButton(text="Напишите автору 📱", url="https://t.me/margaritt1")
         key1.add(btn1)
-        img = open('AVA.jpg', 'rb')
+        img = open('PROFILE.png', 'rb')
         bot.send_photo(message.chat.id, img, reply_markup=key1)
 
     else:  # ...........................................................................................................
